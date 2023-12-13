8bbc170d7778fd0cb5b5ec345aa5217e276d0271
HCodeKeeper <ivannikovmax04@gmail.com>
2023-12-13 17:21:10 +0200

    Added csrfDisable middleware

--- Cameras/settings.py
+++ Cameras/settings.py
@@ -64,11 +64,12 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    #"django.middleware.csrf.CsrfViewMiddleware",
+    "connectivity.middleware.csrf.DisableCSRFMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware"
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 REDIS_URL = config['REDIS_URL']
